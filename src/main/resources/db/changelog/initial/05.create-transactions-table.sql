9dbc9dab41e39468a5461c631103c4f1a23ba2e6
= <=>
2024-08-08 13:52:15 +0300

    Debug transactions table

--- src/main/resources/db/changelog/initial/05.create-transactions-table.sql
+++ src/main/resources/db/changelog/initial/05.create-transactions-table.sql
@@ -1,7 +1,7 @@
 create table transactions(
     id bigserial primary key,
     balance int default 1 check(balance > 0),
-    from_card_id bigint references cards(number),
-    to_card_id bigint references cards(number),
+    from_card_id varchar(16) references cards(number),
+    to_card_id varchar(16) references cards(number),
     transaction_time timestamp not null
 );
