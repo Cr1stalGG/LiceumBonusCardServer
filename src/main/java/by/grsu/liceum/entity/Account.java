45ee94c195794ce69f1b209eba566240a99c9e22
= <=>
2024-09-02 12:52:19 +0300

    Delete useless imports

--- src/main/java/by/grsu/liceum/entity/Account.java
+++ src/main/java/by/grsu/liceum/entity/Account.java
@@ -20,8 +20,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.UuidGenerator;
 
-import java.sql.Date;
-import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
