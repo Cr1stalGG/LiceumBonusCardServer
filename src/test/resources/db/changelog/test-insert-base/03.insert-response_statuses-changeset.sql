71314c103102df964e50d05a664a5e54d0521332
= <=>
2024-09-09 17:23:23 +0300

    update with new statuses

--- src/test/resources/db/changelog/test-insert-base/03.insert-response_statuses-changeset.sql
+++ src/test/resources/db/changelog/test-insert-base/03.insert-response_statuses-changeset.sql
@@ -1,1 +1,5 @@
-insert into response_statuses(id, name, description) values('6d9b69df-8d1e-4a66-b648-c3bfb36ac2c0', 'RESPONSE_STATUS_BONUS_LIFE_ENDED', 'when delete bonuses which time of life is ended');
+insert into response_statuses(id, name, description) values('6d9b69df-8d1e-4a66-b648-c3bfb36ac2c0', 'BONUS_LIFE_ENDED', 'when delete bonuses which time of life is ended');
+insert into response_statuses(id, name, description) values('820e18f5-e831-4185-883d-e409d8a88772', 'TICKET_LIFE_ENDED', 'when delete tickets which time of life is ended');
+insert into response_statuses(id, name, description) values('1a298e92-788a-480b-9add-950126009251', 'ACCOUNT_GRADE_UP', 'grade up of account');
+insert into response_statuses(id, name, description) values('2aa1db1e-964f-476d-8c2f-0500f0f5e8f3', 'ACCOUNT_DELETED', 'account deleted bcs of 12 grade');
+
