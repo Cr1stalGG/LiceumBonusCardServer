b03aec89f362c2af3d0ea696bc107ec1f1a6b4c1
= <=>
2024-09-09 17:26:40 +0300

    update with responses

--- src/main/java/by/grsu/liceum/dto/account/admin/AdminFullDto.java
+++ src/main/java/by/grsu/liceum/dto/account/admin/AdminFullDto.java
@@ -2,12 +2,14 @@ package by.grsu.liceum.dto.account.admin;
 
 import by.grsu.liceum.dto.image.ImageDto;
 import by.grsu.liceum.dto.institution.InstitutionShortcutDto;
+import by.grsu.liceum.dto.response.ResponseShortcutDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
 import java.util.UUID;
 
 @Builder
@@ -25,4 +27,5 @@ public class AdminFullDto {
     private String phoneNumber;
     private InstitutionShortcutDto institution;
     private ImageDto image;//todo check another info
+    private List<ResponseShortcutDto> responses;
 }
--- src/main/java/by/grsu/liceum/dto/mapper/AdminDtoMapper.java
+++ src/main/java/by/grsu/liceum/dto/mapper/AdminDtoMapper.java
@@ -4,11 +4,15 @@ import by.grsu.liceum.dto.account.admin.AdminFullDto;
 import by.grsu.liceum.dto.account.admin.AdminShortcutDto;
 import by.grsu.liceum.dto.image.ImageDto;
 import by.grsu.liceum.dto.institution.InstitutionShortcutDto;
+import by.grsu.liceum.dto.response.ResponseShortcutDto;
 import by.grsu.liceum.entity.Account;
 import by.grsu.liceum.entity.Image;
 import by.grsu.liceum.entity.Institution;
+import by.grsu.liceum.entity.Response;
 import lombok.experimental.UtilityClass;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @UtilityClass
@@ -44,7 +48,17 @@ public class AdminDtoMapper {
                 .phoneNumber(source.getPhoneNumber())
                 .institution(buildInstitution(source.getInstitution()))
                 .image(buildImage(source.getImage()))
+                .responses(buildResponses(source.getResponses()))
                 .build();
+    }
+
+    private static List<ResponseShortcutDto> buildResponses(List<Response> source){
+        if(source == null)
+            return new ArrayList<>();
+        else
+            return source.stream()
+                    .map(ResponseDtoMapper::convertEntityToShortcutDto)
+                    .toList();
     }
 
     private static InstitutionShortcutDto buildInstitution(Institution source) {
